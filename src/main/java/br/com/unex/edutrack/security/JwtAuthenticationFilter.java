364d0b42f589dbd608ce4e516370660fade5babd
Alan-Figueiredo <alancruz495@gmail.com>
2025-10-24 19:57:05 -0300

    fix: code adjustments and refactoring

--- src/main/java/br/com/unex/edutrack/security/JwtAuthenticationFilter.java
+++ src/main/java/br/com/unex/edutrack/security/JwtAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package br.com.unex.edutrack.security;
 
+
+import br.com.unex.edutrack.exception.UnauthorizedException;
 import br.com.unex.edutrack.model.User;
 import br.com.unex.edutrack.repository.UserRepository;
 import jakarta.servlet.FilterChain;
@@ -9,48 +11,70 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import java.io.IOException;
 
-@Component
+
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final TokenService tokenService;
     private final UserRepository userRepository;
+    private final HandlerExceptionResolver handlerExceptionResolver;
 
-    public JwtAuthenticationFilter(TokenService tokenService, UserRepository userRepository) {
+    public JwtAuthenticationFilter(TokenService tokenService, UserRepository userRepository, HandlerExceptionResolver handlerExceptionResolver) {
         this.tokenService = tokenService;
         this.userRepository = userRepository;
+        this.handlerExceptionResolver = handlerExceptionResolver;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        String authHeader = request.getHeader("Authorization");
+        String path = request.getServletPath();
 
-        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+        if (path.startsWith("/swagger-ui")
+                || path.startsWith("/v3/api-docs")
+                || path.startsWith("/actuator")
+                || path.startsWith("/v1/users")
+                || path.startsWith("/error")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+
+        try {
+            String authHeader = request.getHeader("Authorization");
+
+            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                throw new UnauthorizedException("Acesso não autorizado. Token ausente ou inválido.");
+            }
+
             String token = authHeader.replace("Bearer ","").trim();
             String userEmail = tokenService.validateToken(token);
 
-            if( userEmail != null && !userEmail.isEmpty()) {
-                User user = userRepository.findByEmail(userEmail).orElse(null);
-
-                if (user != null) {
-                    UsernamePasswordAuthenticationToken authentication =
-                            new UsernamePasswordAuthenticationToken(user,null,user.getAuthorities());
-                    authentication.setDetails(
-                            new WebAuthenticationDetailsSource().buildDetails(request)
-                    );
-
-                    SecurityContextHolder.getContext().setAuthentication(authentication);
-                }
-
+            if (userEmail == null || userEmail.isEmpty()) {
+                throw new UnauthorizedException("Acesso não autorizado. Token inválido ou expirado.");
             }
-        }
 
-        filterChain.doFilter(request,response);
+            User user = userRepository.findByEmail(userEmail).orElse(null);
+            if (user == null) {
+                throw new UnauthorizedException("Usuário não encontrado para o token fornecido.");
+            }
+
+
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            filterChain.doFilter(request, response); // Continua para o controller
+
+        } catch (UnauthorizedException e) {
+
+            this.handlerExceptionResolver.resolveException(request, response, null, e);
+        }
     }
 }
--- src/main/java/br/com/unex/edutrack/service/SubjectService.java
+++ src/main/java/br/com/unex/edutrack/service/SubjectService.java
@@ -7,26 +7,20 @@ import br.com.unex.edutrack.mapper.subject.SubjectMapper;
 import br.com.unex.edutrack.model.Subject;
 import br.com.unex.edutrack.model.User;
 import br.com.unex.edutrack.repository.SubjectRepository;
-import br.com.unex.edutrack.repository.UserRepository;
-import br.com.unex.edutrack.security.TokenService;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class SubjectService {
 
     private final SubjectRepository subjectRepository;
-    private final UserRepository userRepository;
-    private final SubjectMapper subjectMapper;
-    private final TokenService tokenService;
 
-    public SubjectService(SubjectRepository subjectRepository, UserRepository userRepository, SubjectMapper subjectMapper, TokenService tokenService) {
+    private final SubjectMapper subjectMapper;
+
+
+    public SubjectService(SubjectRepository subjectRepository, SubjectMapper subjectMapper) {
         this.subjectRepository = subjectRepository;
-        this.userRepository = userRepository;
         this.subjectMapper = subjectMapper;
-        this.tokenService = tokenService;
     }
 
     public SubjectResponseDto saveSubject(SubjectRequestDto data){
@@ -38,12 +32,5 @@ public class SubjectService {
         Subject persistedSubject = subjectRepository.save(subject);
 
         return subjectMapper.toSubjectResponseDto(persistedSubject);
-    }
-
-    private void validatedToken(String token){
-
-        String emailUser = tokenService.validateToken(token);
-        Optional<User> user = userRepository.findByEmail(emailUser);
-        System.out.println(user);
     }
 }
